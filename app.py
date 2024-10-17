e1f3b78c932ace9e54cefbc2552af5e9d3b0d06a
venky <venkyeswar2037@gmail.com>
2024-10-17 06:40:41 +0530

    app.py file is updated

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ def predict():
 def prediction():
     return
 if __name__ == "__main__":
-    app.run(debug=True)
-    #  app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
